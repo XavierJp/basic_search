bb90fee986abeaff099bd32cc17a545ed5ba5208
Xavier <xajouppe@gmail.com>
2014-12-16 11:44:55 +0100

    reset et clean

--- Index.py
+++ Index.py
@@ -16,6 +16,15 @@ class Index:
     def __str__(self):
         return self.doc_i, self.word_i
 
+    #frequence normalise. You can use either w_normalized() or tf_idf() in the cos()
+    def w_normalized(self, word, doc_id):
+        indexed_doc = self.doc_i[doc_id]
+        w = indexed_doc[word]
+        w_max = 0
+        for m in indexed_doc:
+            if indexed_doc[m] > w_max: w_max = indexed_doc[m]
+        return float(w)/float(w_max)
+
     #tf_idf
     def tf_idf(self, word, doc_id):
         return self.tf(word, doc_id)*self.idf(word)
@@ -23,8 +32,7 @@ class Index:
     #Log(inverse de la proportion de documents qui contiennent le terme)
     def idf(self, word):
         a = 0
-        for d in self.word_i[str(word)]:
-            a += int(d)
+        for d in self.word_i[str(word)]: a += int(d)
         N = len(self.doc_i.keys())
         return log(float(N)/float(a))
 
@@ -32,8 +40,7 @@ class Index:
     def tf(self, word, doc_id):
         if str(word) in self.doc_i[str(doc_id)]:
             a = self.doc_i[str(doc_id)][str(word)]
-            if a >0:
-                return 1 + log(a)
+            if a >0: return 1 + log(a)
         return 0
 
     #project query_vector over every doc_vector
@@ -60,7 +67,7 @@ class Index:
         n = 0
         for w in indexed_query.keys():
             if w in self.doc_i[doc_id]:
-                p += int(indexed_query[w])*int(self.tf_idf(w,doc_id))
+                p += float(indexed_query[w])*float(self.tf_idf(w,doc_id))
             n += int(indexed_query[w])*int(indexed_query[w])
         if p != 0:
             n = sqrt(n)
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ tps_debut = time.clock()
 cacm = Index_generator()
 
 tps_int1 = time.clock()
-print "cacm file loaded: "+str((tps_int1 - tps_debut))+" sec."
+print "cacm file loaded in "+str((tps_int1 - tps_debut))+" sec."
 
 r = raw_input("your query: ")
 tps_int2 = time.clock()
@@ -18,7 +18,7 @@ result = cacm.index.new_query(r)
 tps_fin = time.clock()
 
 print '------------------------'
-print "query executed in: "+str((tps_fin - tps_int2))+" sec."
+print "query executed in "+str((tps_fin - tps_int2))+" sec."
 print str(len(result))+" result(s) found."
 print '------------------------'
 for doc in reversed(sorted(result.items (), key = operator.itemgetter (1) )):
